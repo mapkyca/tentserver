f1ba99c0727a6a95830f5b2883074db7f4e35841
Marcus Povey <marcus@marcus-povey.co.uk>
2012-08-24 11:41:44 +0100

    Version info; Per domain configuration.

---
+++ configuration/global.php
@@ -0,0 +1,18 @@
+<?php
+
+    /**
+     * Tentserver global configuration file.
+     * 
+     * This is the global configuration file common to all domains on your server.
+     * 
+     * You will almost certainly want to add your settings / overrides to HOSTNAME.php
+     *
+     * @package Tentserver
+     * @subpackage Core
+     */
+
+    // DO NOT EDIT ////
+    global $config;
+    if (!$config) $config = new stdClass();
+    ///////////////////
+    
---
+++ lib/version.info.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Tentserver version info
+     *
+     * @package Tentserver
+     * @subpackage Core
+     */
+	 
+    global $version, $release;
+	
+    $version = 2012082401; // System readable version YYYYMMDDNN (where NN is a daily counter)
+    $release = "1.0"; // Human readable 
